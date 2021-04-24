ded8d41f447b537f20c631d0b5aab17fd83977d2
Abdelfattah Hezema <abdelfatahhezema@gmail.com>
2021-04-24 03:11:31 +0200

    fixing redirection

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -128,6 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = "/store/"
+LOGIN_REDIRECT_URL = "/bookify/index"
 LOGIN_URL = "login"
 MEDIA_URL = "/media/"
